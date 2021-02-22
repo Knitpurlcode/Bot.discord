92875e8df35456f4841ac22657a74d218f70571f
knitpurlcode <lou.petit@gmail.com>
2021-02-22 11:41:59 +0100

    Maj index.js
    Changement serveur et changement du style du Bot

--- index.js
+++ index.js
@@ -13,34 +13,31 @@ bot.emit('guildMemberAdd', 'These params', 'will be logged', 'via the listener.'
 
 // //parler à intervalle régulière
 bot.on('ready', function (){
-    let testChannel = bot.channels.cache.get('765147823720759298');
+    let testChannel = bot.channels.cache.get('813344318198120508');
     console.log("testChannel="+ testChannel);
     setInterval(() => {testChannel.send("@everyone Trop mignon !!")}, 120000);
 })
 
 //Jouer à un jeu
 bot.on('ready', function(){
-    bot.user.setActivity('HelloKitty Online')
+    bot.user.setActivity('Chat perché Online')
     .catch(console.error)
 });
 
 //Répondre à "Chaton"
 bot.on('message', function (message){
-    if (message.content === 'Chaton'){
-        message.reply('I <3 Cats !!!')}
+    if (message.content.toLowerCase().includes('chaton')){
+        message.reply('I eat cats everyday !!!')}
 });
 
 
 //Accueillir les gens
-bot.on('guildMemberAdd', function (welcome){
+bot.on('guildMemberAdd', function (member){
     console.log()
-    let theChannel = bot.channels.cache.get('765147823720759298');
+    let theChannel = bot.channels.cache.get('813344318198120508');
     console.log('theChannel= ' + theChannel)
-    theChannel.send('Olala ! Hiiiiii ! Salut ' + welcome.user.username)
+    theChannel.send('Salut ' + member.user.username + 'ca boum ?')
 });
 
 bot.login(config.BOT_TOKEN);
-
-
-
 
